90bb2c6d02595954e843a838ba583ef94997329b
enunn1 <nunn_ethan@yahoo.com>
2022-04-19 14:24:19 -0500

    Array Practice

---
+++ CoreJavaArrays/src/com/perscholas/java_arrays/JavaArrays.java
@@ -0,0 +1,72 @@
+package com.perscholas.java_arrays;
+
+import java.util.Arrays;
+
+public class JavaArrays {
+
+	public static void main(String[] args) {
+		
+//		int[] numbers = new int[3];
+//		numbers[0] = 1;
+//		numbers[1] = 2;
+//		numbers[2] = 3;
+//		for (int number : numbers) {
+//			System.out.print(number + " ");
+//		}
+//		System.out.println();
+		
+//		int[] numbers2 = {13, 5, 7, 68, 2};
+//		System.out.println(numbers2[2]);
+		
+//		String[] colors = {"red", "green", "blue", "yellow"};
+//		System.out.println(colors.length);
+//		String[] colorsCopy = colors.clone();
+//		System.out.println(Arrays.toString(colorsCopy));
+		
+//		int[] numbers = {21, 16, 9, 57, 46};
+//		System.out.println(numbers[0]);
+//		System.out.println(numbers[numbers.length - 1]);
+//		System.out.println(numbers[numbers.length]);
+//		numbers[5] = 38;
+		
+//		int[] numbers = new int[5];
+//		for (int i = 0; i < 5; i++)
+//		{
+//			numbers[i] = i;
+//		}
+//		System.out.println(Arrays.toString(numbers));
+		
+//		int[] numbers = new int[5];
+//		for (int i = 0; i < 5; i++)
+//		{
+//			numbers[i] = i * 2;
+//		}
+//		System.out.println(Arrays.toString(numbers));
+		
+//		int[] numbers = {12, 34, 56, 78, 90};
+//		for (int i = 0; i < 5; i++)
+//		{
+//			if (i != 2) {
+//				System.out.println(numbers[i]);
+//			}
+//		}
+		
+//		String[] arr = {"ab", "cd", "ef", "gh", "ij"};
+//		System.out.println(Arrays.toString(arr));
+//		String temp = arr[0];
+//		arr[0] = arr[2];
+//		arr[2] = temp;
+//		System.out.println(Arrays.toString(arr));
+		
+//		int[] numbers = {4, 2, 9, 13, 1, 0};
+//		Arrays.sort(numbers);
+//		System.out.println("Array in ascending order: " + Arrays.toString(numbers));
+//		System.out.println("The smallest number is " + numbers[0]);
+//		System.out.println("The biggest number is " + numbers[numbers.length - 1]);
+		
+//		int[] arr = {1, "ab", "cd", "ef", 3.2};
+//		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
--- Java_Practice2/src/main/Main.java
+++ Java_Practice2/src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,24 +9,24 @@ public class Main {
 
 		Scanner userInput = new Scanner(System.in);
 
-		int num = 12;
-		int otherNum = 7;
+//		int num = 11;
+//		int otherNum = 7;
+//
+//		boolean isEven = num % 2 == 0;
+//
+//		if ((num % 2 == 0) && (num % 3 == 0)) {
+//			System.out.println("even from the top");
+//		}
 
-		boolean isEven = num % 2 == 0;
-
-		if ((num % 2 == 0) && (num % 3 == 0)) {
-			System.out.println("even from the top");
-		}
-
-		int age = 21;
-
-		if (age > 15 && age < 18) {
-			System.out.println("in range");
-		} else if (age > 18 & age < 25) {
-			System.out.println("in range over the age of 18");
-		} else {
-			System.out.println("Not in range");
-		}
+//		int age = 21;
+//
+//		if (age > 15 && age < 18) {
+//			System.out.println("in range");
+//		} else if (age > 18 & age < 25) {
+//			System.out.println("in range over the age of 18");
+//		} else {
+//			System.out.println("Not in range");
+//		}
 
 //		System.out.println("Hi, what is your name? ");
 //		String firstName = userInput.nextLine();
@@ -39,27 +40,27 @@ public class Main {
 //			System.out.println("Sorry " + firstName + ", you are not old enough");
 //		}
 
-		userInput.close();
+//		userInput.close();
 
-		int day = 1;
-
-		switch (day) {
-		case 1:
-			System.out.println("Mon");
-			break;
-		case 2:
-			System.out.println("Tues");
-			break;
-		case 3:
-			System.out.println("Wed");
-			break;
-		case 4:
-			System.out.println("Thur");
-			break;
-		default:
-			System.out.println("On break");
-			break;
-		}
+//		int day = 1;
+//
+//		switch (day) {
+//		case 1:
+//			System.out.println("Mon");
+//			break;
+//		case 2:
+//			System.out.println("Tues");
+//			break;
+//		case 3:
+//			System.out.println("Wed");
+//			break;
+//		case 4:
+//			System.out.println("Thur");
+//			break;
+//		default:
+//			System.out.println("On break");
+//			break;
+//		}
 
 		// ternary operator
 
@@ -88,12 +89,87 @@ public class Main {
 //		for (int i = 0, j = 0; (i + j < 10); i++, j++) {
 //			System.out.println(i + j);
 //		}
-		for (int i = 10; i > 0; i--) {
-			System.out.println("count " + i);
-			for (int j = 1; j <= 5; j *= 2) {
-				System.out.println(j);
+//		for (int i = 10; i > 0; i--) {
+//			System.out.println("count " + i);
+//			for (int j = 1; j <= 5; j *= 2) {
+//				System.out.println(j);
+//			}
+//		}
+		
+//		String[] myStr = new String[10];
+//		String[] myStr2 = {"Ethan", "hello"};
+//		myStr[0]= "Ethan";
+//		myStr[1]= "hello";
+//		myStr[2]= myStr[0] + " " + myStr[1];
+//		System.out.print(myStr[2]);
+		
+//		int[] myInt = new int[10];
+//		for (int i = 1; i < 10; i++) {
+//			myInt[i] = i + myInt[i-1];
+////			System.out.println(myInt[i]);
+//		}
+		
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println(myInt[i]);
+//		}
+		
+		// Enhanced for loop
+//		for (int i: myInt) {
+//			System.out.println(i);
+//		}
+		
+//		int[] copyOfArr = myInt.clone();
+//		
+//		myInt[0] = 234;
+//		
+//		System.out.println("copy: " + Arrays.toString(copyOfArr));
+//		System.out.println("og: "+ Arrays.toString(myInt));
+		
+//		int n;
+//		System.out.println("Enter the number of elements you want to store: ");
+//		
+//		n = userInput.nextInt();
+//		
+//		int[] arr = new int[n];
+//		System.out.println("Enter the elements of the array: ");
+//		
+//		for (int i = 0; i < n; i++) {
+//			arr[i]=userInput.nextInt();
+//		}
+//		
+//		System.out.println("Array elements are: ");
+//		for (int i=0; i<n; i++) {
+//			System.out.println(arr[i]); 
+//		}
+		
+//		double[] arr = new double[5];
+//		for (int i = 0; i < 5; i++) {
+//			arr[i] = Math.floor(Math.random() * 10);
+//		}
+//		
+//		System.out.println(Arrays.toString(arr));
+		
+		int[] numbers = {12, 3, 4, 5, -5, 0};
+		
+//		int sum = 0;
+//		
+//		for (int number : numbers) {
+//			sum += number;
+//		}
+//		
+//		System.out.print(sum);
+		
+		double max = numbers[0];
+		int maxIdx = 0;
+		for(int i = 0; i < numbers.length; i++) {
+			if (numbers[i] > max) {
+				max = numbers[i];
+				maxIdx = i;
 			}
 		}
+		
+		System.out.print(maxIdx);
+		
 	}
 
 }
